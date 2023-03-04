453459cd839fb8e636589216688bfba265eaf693
Mark <markstrudwick@outlook.com>
2023-03-04 13:01:53 +0000

    Refactor the index page and add a 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+export default function Custom404() {
+  return (
+    <>
+      <h1>404 Error: Page Not Found</h1>
+      <p>
+        But on the bright side, at least you found the top-secret hidden page!
+        Just kidding, I don&apos;t have one of those...
+      </p>
+    </>
+  );
+}
